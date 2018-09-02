9d0cb9f44c8504baf8fc9773d8b55c7dfcdf54e5
JPG-Consulting <opensource@jpg-consulting.com>
2018-09-02 18:18:52 +0200

    Initial MVC setup with MySQL

---
+++ Invoices.Site/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Invoices.Site.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
